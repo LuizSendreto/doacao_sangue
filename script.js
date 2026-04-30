89cb3553407ec474586ad2c1b445e9effb0414f6
LuizSendreto <luiz_s_dos-santos@estudante.sesisenai.org.br>
2026-04-30 08:21:58 -0300

    add - coloquei a o js denovo, pois apagaram

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById("formDoacao");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const idade = +document.getElementById("idade").value;
+    const peso = +document.getElementById("peso").value;
+    const cidade = document.getElementById("cidade").value;
+    const estado = document.getElementById("Estado").value;
+    const sanguineo = document.getElementById("sanguineo").value;
+
+    if (nome.length < 3) return alert("Nome inválido");
+    if (!email.includes("@")) return alert("Email inválido");
+    if (telefone.length < 10) return alert("Telefone inválido");
+    if (idade <= 16) return alert("Idade minima 16 anos");
+    if (!sanguineo) return alert("Selecione um tipo sanguineo");
+    if (peso < 50) return alert("Peso baixo demais para a doação.");
+    if (!cidade) return alert("Informe a cidade");
+    if (!estado) return alert("Informe o estado");
+
+    let informações = [{
+        nome,
+        email,
+        idade,
+        peso,
+        tipoSanguineo: sanguineo,
+        telefone,
+        cidade,
+        estado
+    }];
+
+    alert("Formulário enviado!");
+    alert(JSON.stringify(informações, null, ));
+});
